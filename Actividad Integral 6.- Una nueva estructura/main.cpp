1520e3a297d9060d59ab7afc497c2715cc32c3bd
A01706219 <58478983+A01706219@users.noreply.github.com>
2020-12-04 14:53:21 -0600

    Update main.cpp

--- Actividad Integral 6.- Una nueva estructura/main.cpp
+++ Actividad Integral 6.- Una nueva estructura/main.cpp
@@ -6,47 +6,83 @@
 using namespace std;
 
 int main() {
-    cout << "Rodrigo Marquina Magana - A01706210" << endl;
-    cout << "Bienvenido a la actividad integral extra: " << endl;
-    cout << "Caso 1 de prueba: " << endl;
-    cout << "Los valores 0.3, 0.2, 0.7, 0.9, 0.1 y 0.9 seran arreglados de menor a mayor por el metodo bucket sort." << endl;
-    float lista[] = {0.3, 0.2, 0.7, 0.9, 0.1, 0.9}; //Inicializamos la lista con los valores del caso de prueba
 
-    int longitud = sizeof(lista)/sizeof(lista[0]); //La longitud se pasa de bits a entero
+    int opcion = 0;
+    cout << "¿Que quiere hacer?" << endl;
+    cout << "Opcion 1: Ver casos de prueba" << endl;
+    cout << "Opcion 2: Ingresar lista" << endl;
+    cin >> opcion;
 
-    cout << "Lista sin ordenar: " << endl;
-    for(int i = 0; i < longitud; i++){ //Tomamos los valores de la lista y los imprimimos para que los vea el usuario
-       cout << lista[i] << ", ";
-    }
-    cout << '\n';
-    bucketSort(lista, longitud); //Con la funcion ordenamos la lista
+    if(opcion == 1){
+      cout << "Rodrigo Marquina Magana - A01706210" << endl;
+      cout << "Bienvenido a la actividad integral extra: " << endl;
+      cout << "Caso 1 de prueba: " << endl;
+      cout << "Los valores 0.3, 0.2, 0.7, 0.9, 0.1 y 0.9 seran arreglados de menor a mayor por el metodo bucket sort." << endl;
+      float lista[] = {0.3, 0.2, 0.7, 0.9, 0.1, 0.9}; //Inicializamos la lista con los valores del caso de prueba
 
-    cout << "Lista ordenada: " << endl; //Se la enviamos al usuario par que la vea
-    for (int i = 0; i < longitud; i++){
-       cout << lista[i] << ", ";
-    }
+      int longitud = sizeof(lista)/sizeof(lista[0]); //La longitud se pasa de bits a entero
 
-    cout << '\n' << "Enter para continuar." << endl; //Caso de prueba 2
-    cin.ignore();
+      cout << "Lista sin ordenar: " << endl;
+      for(int i = 0; i < longitud; i++){ //Tomamos los valores de la lista y los imprimimos para que los vea el usuario
+         cout << lista[i] << ", ";
+      }
+      cout << '\n';
+      bucketSort(lista, longitud); //Con la funcion ordenamos la lista
 
-    cout << "Caso 2 de prueba: " << endl;
-    cout << "Los valores 0.32, 0.51, 0.95, 0.29, 0.01, 0.23 y 0.43 seran arreglados de menor a mayor por el metodo bucket sort." << endl;
+      cout << "Lista ordenada: " << endl; //Se la enviamos al usuario par que la vea
+      for (int i = 0; i < longitud; i++){
+         cout << lista[i] << ", ";
+      }
 
-    float lista2[] = {0.32, 0.51, 0.95, 0.29, 0.01, 0.23, 0.43};
+      cout << '\n' << "Enter para continuar." << endl; //Caso de prueba 2
+      cin.ignore();
 
-    int longitud2 = sizeof(lista)/sizeof(lista[0]);
+      cout << "Caso 2 de prueba: " << endl;
+      cout << "Los valores 0.32, 0.51, 0.95, 0.29, 0.01, 0.23 y 0.43 seran arreglados de menor a mayor por el metodo bucket sort." << endl;
 
-    cout << "Lista sin ordenar: " << endl;
-    for(int i = 0; i < longitud2; i++){
-       cout << lista2[i] << ", ";
-    }
-    cout << '\n';
-    bucketSort(lista2, longitud2);
+      float lista2[] = {0.32, 0.51, 0.95, 0.29, 0.01, 0.23, 0.43};
 
-    cout << "Lista ordenada: " << endl;
-    for (int i = 0; i < longitud2; i++){
-       cout << lista2[i] << ", ";
+      int longitud2 = sizeof(lista)/sizeof(lista[0]);
+
+      cout << "Lista sin ordenar: " << endl;
+      for(int i = 0; i < longitud2; i++){
+         cout << lista2[i] << ", ";
+      }
+      cout << '\n';
+      bucketSort(lista2, longitud2);
+
+      cout << "Lista ordenada: " << endl;
+      for (int i = 0; i < longitud2; i++){
+         cout << lista2[i] << ", ";
+      }
+    } else{
+
+      int longitud3 = 5;
+
+      float lista3[] = {};
+
+      cout << "Ingresa 5 numeros" << endl;
+
+      for(int i = 0; i < 5; i++){
+        cout << "Numero " << endl;
+        cin >> lista3[i];
+      }
+
+      cout << "Lista sin ordenar: " << endl;
+      for(int i = 0; i < longitud3; i++){ //Tomamos los valores de la lista y los imprimimos para que los vea el usuario
+         cout << lista3[i] << ", ";
+      }
+      cout << '\n';
+      bucketSort(lista3, longitud3); //Con la funcion ordenamos la lista
+
+      cout << "Lista ordenada: " << endl; //Se la enviamos al usuario par que la vea
+      for (int i = 0; i < longitud3; i++){
+         cout << lista3[i] << ", ";
+      }
+
+
     }
 
     return 0;
 }
+
